eedffc97338ae39d3adb2085cf42b5cd4126d715
Brayden Bowman <81596510+thaos2713@users.noreply.github.com>
2023-02-22 21:39:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+'''
+CS 620 Project
+Authors: Christopher Sanders, Brayden Bowman, Ismail Rabiu
+'''
 
+# Imports
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Define main function
+def main():
+    data = pd.read_csv('./data.csv')
+
+# Execute main() function
+if __name__ == '__main__':
+    main()
