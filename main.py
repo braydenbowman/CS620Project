a4b7fd36c27455bbc2e2475e2bd31d5e95685e4a
Brayden Bowman <81596510+thaos2713@users.noreply.github.com>
2023-02-26 11:47:22 -0500

    Added Functions

--- main.py
+++ main.py
@@ -12,13 +12,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Define 
+def analyzeWinLose(data):
+    data1 = data
+    data1 = data1.assign(WR=lambda x : x.WHG - x.WAG)
+    plt.hist(data1[:-1], data1, weights=counts)
+    return
+
+# Define 
+def analyzeEfficiency(data):
+    return
+
+# Define 
+def analyzeFreeThrows(data):
+    return
+
+# Define 
+def analyzeTurnovers(data):
+    return
+
 # Define main function
 def main():
     data = pd.read_csv(sys.argv[1], index_col=0)
-    data1 = data[]
-    data1 = data1.assign(WR=lambda x : x.WHG - x.WAG)
-    print(data1)
-
+    analyzeWinLose(data)
+    analyzeEfficiency(data)
+    analyzeFreeThrows(data)
+    analyzeTurnovers(data)
+    
 # Execute main() function
 if __name__ == '__main__':
     main()
